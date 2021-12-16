288a0450e86ddf182f0603558ee34810c6b047f2
elgatogt <wyliancabezas@gmail.com>
2021-12-15 20:53:37 -0600

    Add files via upload
    Se agrega codigo base

---
+++ src/main/java/com/proyecto/intouch2/Intouch2Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.intouch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intouch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Intouch2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/intouch2/Intouch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.intouch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Intouch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
